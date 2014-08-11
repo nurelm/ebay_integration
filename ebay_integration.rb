14c14725f4fd404e07ba8b751e332148ea5f32e5
nishant-cyro <nishant.tuteja@vinsol.com>
2014-08-12 00:39:07 +0530

    add return before returning a value

--- ebay_integration.rb
+++ ebay_integration.rb
@@ -3,38 +3,38 @@ require 'json'
 
 class EbayIntegration < Sinatra::Base 
   get '/' do 
-    200, "Coming Soon..." 
+    return 200, "Coming Soon..." 
   end
 
   get '/get_products' do 
-    200, "Coming Soon..." 
+    return 200, "Coming Soon..." 
   end
 
   post '/get_orders' do 
-    200, "Coming Soon..." 
+    return 200, "Coming Soon..." 
   end
 
   post '/get_products' do 
-    200, "Coming Soon..." 
+    return 200, "Coming Soon..." 
   end
 
   post '/set_inventory' do 
-    200, "Coming Soon..." 
+    return 200, "Coming Soon..." 
   end
 
   post '/add_product' do 
-    200, "Coming Soon..." 
+    return 200, "Coming Soon..." 
   end
 
   post '/update_product' do 
-    200, "Coming Soon..." 
+    return 200, "Coming Soon..." 
   end
 
   post '/add_shipment' do 
-    200, "Coming Soon..." 
+    return 200, "Coming Soon..." 
   end
 
   post '/update_shipment' do 
-    200, "Coming Soon..." 
+    return 200, "Coming Soon..." 
   end
 end

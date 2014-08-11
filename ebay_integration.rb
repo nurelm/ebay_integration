a90e8679fb9d691490c6e6dc712c112ea85e342e
nishant-cyro <nishant.tuteja@vinsol.com>
2014-08-12 00:41:29 +0530

    modify return value of routes

--- ebay_integration.rb
+++ ebay_integration.rb
@@ -3,38 +3,38 @@ require 'json'
 
 class EbayIntegration < Sinatra::Base 
   get '/' do 
-    return 200, "Coming Soon..." 
+    "Coming Soon..." 
   end
 
   get '/get_products' do 
-    return 200, "Coming Soon..." 
+    "Coming Soon..." 
   end
 
   post '/get_orders' do 
-    return 200, "Coming Soon..." 
+    "Coming Soon..." 
   end
 
   post '/get_products' do 
-    return 200, "Coming Soon..." 
+    "Coming Soon..." 
   end
 
   post '/set_inventory' do 
-    return 200, "Coming Soon..." 
+    "Coming Soon..." 
   end
 
   post '/add_product' do 
-    return 200, "Coming Soon..." 
+    "Coming Soon..." 
   end
 
   post '/update_product' do 
-    return 200, "Coming Soon..." 
+    "Coming Soon..." 
   end
 
   post '/add_shipment' do 
-    return 200, "Coming Soon..." 
+    "Coming Soon..." 
   end
 
   post '/update_shipment' do 
-    return 200, "Coming Soon..." 
+    "Coming Soon..." 
   end
 end

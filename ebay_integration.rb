a7e9576cc990349a54366372f48b4269504141b8
nishant-cyro <nishant.tuteja@vinsol.com>
2014-08-12 00:17:19 +0530

    add more routes and specify them as coming soon

--- ebay_integration.rb
+++ ebay_integration.rb
@@ -3,14 +3,38 @@ require 'json'
 
 class EbayIntegration < Sinatra::Base 
   get '/' do 
-    "Hello World" 
+    200, "Coming Soon..." 
   end
 
   get '/get_products' do 
-    "Hello World" 
+    200, "Coming Soon..." 
+  end
+
+  post '/get_orders' do 
+    200, "Coming Soon..." 
   end
 
   post '/get_products' do 
-    "Hello World" 
+    200, "Coming Soon..." 
+  end
+
+  post '/set_inventory' do 
+    200, "Coming Soon..." 
+  end
+
+  post '/add_product' do 
+    200, "Coming Soon..." 
+  end
+
+  post '/update_product' do 
+    200, "Coming Soon..." 
+  end
+
+  post '/add_shipment' do 
+    200, "Coming Soon..." 
+  end
+
+  post '/update_shipment' do 
+    200, "Coming Soon..." 
   end
 end

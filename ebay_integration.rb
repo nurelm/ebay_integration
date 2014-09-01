9a73a630cf356647204daa5d3f7f1ba51ec5787a
nishant-cyro <nishant.tuteja@vinsol.com>
2014-09-01 20:40:44 +0530

    use add_object in add_product

--- ebay_integration.rb
+++ ebay_integration.rb
@@ -30,7 +30,7 @@ class EbayIntegration < EndpointBase::Sinatra::Base
   post '/add_product' do
     response = Ebay.new(@payload, @config).add_product
     if response.success?
-      add_value 'ebay_item_id', response.payload[:item_id]
+      add_object 'product', { product: @payload[:product], item_id: response.payload[:item_id] }
       result 200, "Product with #{ response.payload[:item_id] } is added to eBay."
     else
       result 500, response.errors.first.long_message
